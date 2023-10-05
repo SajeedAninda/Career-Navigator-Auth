515e6d98d34ead77b3d0753c5baa49568df09585
SajeedAninda <sajaninda@gmail.com>
2023-10-05 20:19:51 +0600

    Enabled Github Login

--- Auth/AuthProvider.jsx
+++ Auth/AuthProvider.jsx
@@ -2,14 +2,17 @@ import React, { createContext } from 'react';
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
 const auth = getAuth(app);
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider,GithubAuthProvider  } from "firebase/auth";
 const googleProvider = new GoogleAuthProvider();
 export let AuthContext = createContext(null);
-
+const githubProvider = new GithubAuthProvider();
 
 const AuthProvider = ({ children }) => {
     let googleLogin = () => {
         return signInWithPopup(auth, googleProvider);
+    }
+    let githubLogin=()=>{
+        return signInWithPopup(auth, githubProvider);
     }
     let register=(email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password)
@@ -22,11 +25,11 @@ const AuthProvider = ({ children }) => {
 
 
 
-
     let authorization={
         googleLogin,
         register,
-        login
+        login,
+        githubLogin
     }
     return (
         <AuthContext.Provider value={authorization}>
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,7 @@ import { useContext } from 'react';
 import { AuthContext } from '../../Auth/AuthProvider';
 
 const Login = () => {
-    let { googleLogin, login } = useContext(AuthContext);
+    let { googleLogin, login, githubLogin } = useContext(AuthContext);
     let navigate = useNavigate();
 
     let handleGoogle = () => {
@@ -20,16 +20,23 @@ const Login = () => {
         e.preventDefault();
         let email = e.target.email.value;
         let password = e.target.password.value;
-        login(email,password)
+        login(email, password)
             .then((userCredential) => {
                 navigate("/");
             })
             .catch((error) => {
                 console.log(error);
             });
-
     }
 
+    let handleGithub = () => {
+        githubLogin()
+            .then((result) => {
+                navigate("/");
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
 
     return (
         <div class="">
@@ -38,7 +45,7 @@ const Login = () => {
                     <p class="text-center text-sm text-gray-400 font-light">Sign in with</p>
                     <div>
                         <div class="flex items-center justify-center space-x-4 mt-3">
-                            <button
+                            <button onClick={handleGithub}
                                 class="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-[#85D7A9] border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
                             >
                                 <svg
