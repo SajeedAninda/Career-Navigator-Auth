de2fe8be0bedc6e991361fa916adba24c3cecd72
SajeedAninda <sajaninda@gmail.com>
2023-10-05 19:08:03 +0600

    Created Registration Page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
                     <div>
                         <div class="flex items-center justify-center space-x-4 mt-3">
                             <button
-                                class="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                                class="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-[#85D7A9] border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -25,7 +25,7 @@ const Login = () => {
                                 Github
                             </button>
                             <button
-                                class="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                                class="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-[#85D7A9] border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -109,7 +109,7 @@ const Login = () => {
                         </div>
                         <div class="flex items-center justify-center mt-8">
                             <button
-                                class="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                                class="text-white py-2 px-4 uppercase rounded bg-[#85D7A9] hover:bg-[#85D7A9] shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
                             >
                                 Sign in
                             </button>
